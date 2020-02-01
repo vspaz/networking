73d83298fafd366b0df8e4a39953e0f2026fc5af
vspaz <vladimir.edelman@gmail.com>
2020-02-01 16:56:41 +0100

    add multithreaded gzipper

---
+++ src/multithreading/gzipping/GzipFiles.java
@@ -0,0 +1,28 @@
+package gzipping;
+
+import java.nio.file.Paths;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class GzipFiles {
+    public final static int THREAD_COUNT = 4;
+
+    public static void main(String[] args) {
+        ExecutorService pool = Executors.newFixedThreadPool(GzipFiles.THREAD_COUNT);
+        List<String> files = new ArrayList<>();
+        String path = Paths.get(".").toAbsolutePath().normalize().toString();
+        files.add(path + "/digest/foo.txt");
+        files.add(path + "/digest/bar.txt");
+        for (String filePath : files) {
+            File file = new File(filePath);
+            if (file.exists()) {
+                Runnable task = new GzipRunnable(file);
+                pool.submit(task);
+            }
+        }
+        pool.shutdown();
+    }
+}
