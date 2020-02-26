78b63e2c0d13e90898d76e6d29a7bad895c6f1d8
vspaz <vladimir.edelman@gmail.com>
2020-02-26 16:33:47 +0100

    make getCount synchronized too

--- src/multithreading/counting/SynchronizedCounter.java
+++ src/multithreading/counting/SynchronizedCounter.java
@@ -6,6 +6,13 @@ public class SynchronizedCounter extends Counter {
         super();
     }
 
+    @Override
+    public synchronized int getCount() {
+        // should be also synchronized not to occasionally return the stale value!!!
+        return count;
+    }
+
+    @Override
     public synchronized void increment() {
         count++;
     }
