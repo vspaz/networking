5430ed4d27940d48ad0f6d1fd2ab7cf7366eb7d6
vspaz <vladimir.edelman@gmail.com>
2020-02-26 15:11:13 +0100

    simpleThread added

---
+++ src/multithreading/simplethreading/SimpleThread.java
@@ -0,0 +1,24 @@
+package simplethreading;
+
+// can be replaced with new Thread(() -> System.out.println("In worker"));
+class Worker extends Thread {
+    @Override
+    public void run() {
+        System.out.println("In worker");
+    }
+}
+
+
+public class SimpleThread {
+    public static void main(String[] args) {
+        Worker worker = new Worker();
+        worker.start();
+        Thread.yield();
+        System.out.println("In main thread");
+        try {
+            worker.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
